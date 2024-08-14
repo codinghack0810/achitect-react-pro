101d19c5e850ec94c603c9dd06f739319162ece8
codinghack0810 <codinghack30@gmail.com>
2024-08-15 05:52:48 +1000

    version1

---
+++ src/Layout/AppHeader/index.js
@@ -0,0 +1,62 @@
+import React, { Fragment } from "react";
+import cx from "classnames";
+
+import { connect } from "react-redux";
+
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
+import HeaderLogo from "../AppLogo";
+
+import SearchBox from "./Components/SearchBox";
+import MegaMenu from "./Components/MegaMenu";
+import UserBox from "./Components/UserBox";
+
+import HeaderDots from "./Components/HeaderDots";
+
+class Header extends React.Component {
+  render() {
+    let {
+      headerBackgroundColor,
+      enableMobileMenuSmall,
+      enableHeaderShadow,
+    } = this.props;
+    return (
+      <Fragment>
+        <TransitionGroup>
+          <CSSTransition component="div"
+            className={cx("app-header", headerBackgroundColor, {
+              "header-shadow": enableHeaderShadow,
+            })}
+            appear={true} timeout={1500} enter={false} exit={false}>
+            <div>
+              <HeaderLogo />
+              <div className={cx("app-header__content", {
+                  "header-mobile-open": enableMobileMenuSmall,
+                })}>
+                <div className="app-header-left">
+                  <SearchBox />
+                  <MegaMenu />
+                </div>
+                <div className="app-header-right">
+                  <HeaderDots />
+                  <UserBox />
+                </div>
+              </div>
+            </div>
+          </CSSTransition>
+        </TransitionGroup>
+      </Fragment>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  enableHeaderShadow: state.ThemeOptions.enableHeaderShadow,
+  closedSmallerSidebar: state.ThemeOptions.closedSmallerSidebar,
+  headerBackgroundColor: state.ThemeOptions.headerBackgroundColor,
+  enableMobileMenuSmall: state.ThemeOptions.enableMobileMenuSmall,
+});
+
+const mapDispatchToProps = (dispatch) => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
